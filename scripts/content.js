c977f80c3fd5b97164a2688320f49a2c307ed3ec
Sam Justé <sam.juste@cegeka.com>
2024-03-13 12:00:46 +0100

    added observer to make sure the class gets removed after all API calls

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,21 @@
 const woField = document.getElementById('search-workorder');
 
-woField.classList.remove('disabled');
+var prevClassState = woField.classList.contains('disabled');
+var observer = new MutationObserver(function(mutations) {
+                    mutations.forEach(function(mutation) {
+                        if(mutation.attributeName == "class"){
+                            var currentClassState = mutation.target.classList.contains('disabled');
+                            if(prevClassState !== currentClassState)    {
+                                prevClassState = currentClassState;
+                                if(currentClassState){
+                                    console.log("class added!");
+                                    woField.classList.remove('disabled');
+                                }
+                                else 
+                                    console.log("class removed!");
+                            }
+                        }
+                    });
+                });
+observer.observe(woField, {attributes: true});
+
